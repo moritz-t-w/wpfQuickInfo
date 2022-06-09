478316f10fd30da043ab77ded8d555f3e714ebe8
MoDerPo <90388353+moritz-t-w@users.noreply.github.com>
2022-06-09 12:14:09 +0200

    Created getGroups function
    This took me with the generous help of my friends a while.
    
    Co-Authored-By: dcrime <20734229+dcrime@users.noreply.github.com>
    Co-Authored-By: Splatterxl <splatterxl@outlook.ie>

--- script.js
+++ script.js
@@ -1,22 +1,73 @@
-function getElementByXpath(path) {
-    return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-}
-function getContents(subPath) {
-    return getElementByXpath("/html/body/div[3]/div[1]/section/div[1]/div[1]/div[1]/div/div/div/div/div/div/div[9]/"+subPath).innerHTML.trim();
-}
-
-let groups = {
-    1: {
-        tuesday: {
-            date: getContents("div[1]/div/div[1]/div[1]"),
-            location: getContents("div[1]/div/div[1]/div[3]"),
-            info: getContents("div[1]/div/div[2]/div")
-        },
-        thursday: {
-            date: getContents("div[2]/div/div[1]/div[1]"),
-            location: getContents("div[2]/div/div[1]/div[3]"),
-            info: getContents("div[2]/div/div[2]/div")
-        },
+class Day{
+    date = "";
+    location = "";
+    info = "";
+    constructor(date, location, info){
+        this.date = date;
+        this.location = location;
+        this.info = info;
     }
 }
-console.log(groups)
+
+class Week {
+    tuesday = {};
+    thursday = {};
+    
+    constructor(tuesday, thursday){
+        this.tuesday = tuesday;
+        this.thursday = thursday;
+    }
+}
+
+class Group {
+    weeks = [];
+
+    constructor(weeks){
+        this.weeks = weeks;
+    }
+}
+
+const htmlClassNames = new Day("price-title feature-title","amount-price amount feature-title","subtitle")
+
+function getGroups() {
+    let dateGroups = elementsToArrays("date");
+    let locationGroups = elementsToArrays("location");
+    let infoGroups = elementsToArrays("info");
+
+    let groups = [];
+    dateGroups.forEach(dateWeeks => {
+        let group = new Group([]);
+        for (let i = 0; i < dateWeeks.length; i++) {
+            group.weeks.push(new Week(
+                new Day(
+                    dateWeeks[0][0],
+                    locationGroups[i][0][0],
+                    infoGroups[i][0][0]
+                ),
+                new Day(
+                    dateWeeks[0][1],
+                    locationGroups[i][0][1],
+                    infoGroups[i][0][1]
+                )
+            ));
+        }
+        groups.push(group)
+    });
+}
+
+function elementsToArrays(property){
+    let values = [];
+    for (const element of document.getElementsByClassName(htmlClassNames[property])) {
+        values.push(element.innerHTML);
+    }
+    const weeks = chunkArray(values, 2);
+    return chunkArray(weeks, weeks.length/2);
+}
+
+function chunkArray(array, chunkSize) {
+    let result = [];
+    while(array.length > 0) {
+        result.push(array.splice(0, chunkSize));
+    }
+    return result
+}
